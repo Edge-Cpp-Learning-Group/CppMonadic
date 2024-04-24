39609aa1866431604906c3be9e4dc5385abcd95f
Wei Wei <shazhouke@gmail.com>
2024-04-24 08:04:41 +0800

    Add more test cases about Maybe monad

--- maybe.h
+++ maybe.h
@@ -8,13 +8,18 @@ private:
   Maybe(const std::optional<T> &opt): opt_(opt) { }
 public:
   Maybe(): Maybe(std::nullopt) { }
+  // Monad::Pure
   Maybe(const T &val): Maybe(std::optional(val)) { }
+  // Functor::Map
   template <typename F, typename U>
   Maybe(const F f, const Maybe<U> &val): Maybe(val.HasValue() ? std::optional(f(val.Value())) : std::nullopt) { }
+  // Monad::Join
   Maybe(const Maybe<Maybe<T>> &mmVal): Maybe(mmVal.HasValue() ? mmVal.Value().opt_ : std::nullopt) { }
 
   const T &Value() const { return opt_.value(); }
   bool HasValue() const { return opt_.has_value(); }
+
+  bool operator == (const Maybe &mVal) const { return opt_ == mVal.opt_; }
 
 private:
   std::optional<T> opt_;
--- test/maybe.cpp
+++ test/maybe.cpp
@@ -2,7 +2,67 @@
 #include "../monad.h"
 #include "../maybe.h"
 
-TEST_CASE("Maybe as Monad", "[Maybe]") {
+TEST_CASE("Map as Functor", "[Maybe]") {
+  auto x = Maybe<int>(0);
+  auto y = Maybe<int>();
+
+  auto z = Monad<Maybe>::Map([](int n) { return n + 1; }, x);
+
+  REQUIRE(z.HasValue() == true);
+  REQUIRE(z.Value() == 1);
+
+  auto w = Monad<Maybe>::Map([](int n) { return n + 1; }, y);
+  REQUIRE(w.HasValue() == false);
+}
+
+TEST_CASE("Bind as Monad", "[Maybe]") {
+  auto x = Maybe<int>(0);
+  auto y = Maybe<int>();
+
+  auto z = Monad<Maybe>::Bind(x, [](int n) { return Maybe(n + 1); });
+
+  REQUIRE(z.HasValue() == true);
+  REQUIRE(z.Value() == 1);
+
+  auto w = Monad<Maybe>::Bind(y, [](int n) { return Maybe(n + 1); });
+  REQUIRE(w.HasValue() == false);
+
+  auto u = Monad<Maybe>::Bind(x, [](int n) { return Maybe<int>(); });
+  REQUIRE(u.HasValue() == false);
+}
+
+TEST_CASE("Join as Monad", "[Maybe]") {
+  auto x = Monad<Maybe>::Join(Maybe<Maybe<int>>(Maybe(1)));
+  REQUIRE(x.HasValue() == true);
+  REQUIRE(x.Value() == 1);
+
+  auto y = Monad<Maybe>::Join(Maybe<Maybe<int>>(Maybe<int>()));
+  REQUIRE(y.HasValue() == false);
+
+  auto z = Monad<Maybe>::Join(Maybe<Maybe<int>>());
+  REQUIRE(z.HasValue() == false);
+}
+
+TEST_CASE("Sugar with operator >>", "[Maybe]") {
+  auto x = Maybe<int>(1);
+
+  auto y = x >> [](int n) { return n + 1; };
+  REQUIRE(y.HasValue() == true);
+  REQUIRE(y.Value() == 2);
+
+  auto z = y >> [](int n) { return Maybe(n * 2); }
+             >> [](int n) { return n + 1; };
+  
+  REQUIRE(z.HasValue() == true);
+  REQUIRE(z.Value() == 5);
+
+  auto w = z >> [](int n) { return Maybe(n * 2); }
+             >> [](int) { return Maybe<int>(); };
+
+  REQUIRE(w.HasValue() == false);
+}
+
+TEST_CASE("Lift Calls", "[Maybe]") {
   auto x = Maybe<int>(0);
   auto y = Maybe<int>();
   auto z = Maybe<int>(2);
